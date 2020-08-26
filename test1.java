4cb2c23c174369d8b5aac53f83b7aa48307eaf4c
PRITHVI RAJ KARKI <61450572+prithvi8117@users.noreply.github.com>
2020-08-26 12:50:08 +0530

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,7 @@
+class test1
+{
+  public static void main(String[] a)
+  {
+    Syatem.out.println("Congratulations, We are in Java Program !!!\n\nWe can Write our java script like this !!!");
+  }
+}
