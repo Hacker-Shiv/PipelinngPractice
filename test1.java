e075d425d3b12185f10a476f7d23757792891047
PRITHVI RAJ KARKI <61450572+prithvi8117@users.noreply.github.com>
2020-08-26 12:57:00 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -2,6 +2,6 @@ class test1
 {
   public static void main(String[] a)
   {
-    Syatem.out.println("Congratulations, We are in Java Program !!!\n\nWe can Write our java script like this !!!");
+    System.out.println("Congratulations, We are in Java Program !!!\n\nWe can Write our java script like this !!!");
   }
 }
